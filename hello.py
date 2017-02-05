01cdec9477989b2434f754ab9b496e08808d7c12
creepysynth <creepysynth@gmail.com>
2017-02-05 13:54:06 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,7 +8,6 @@ def cookie_counter(environ):
     if 'HTTP_COOKIE' in environ:
         counter = environ['HTTP_COOKIE'].split('=')[1]
         counter = int(counter) + 1
-
     return counter
 
 def get(environ):
@@ -21,11 +20,14 @@ def get(environ):
           <meta charset="utf-8">
         </head>
         <body>
-        <table border="2">
-          <tr>
-            <th>Параметр</th>
-            <th>Значение</th>
-          </tr>
+          <table border="2">
+            <tr>
+              <th>Параметр</th>
+              <th>Значение</th>
+            </tr>
+            {rows}
+          </table>
+        </body>
     """
     row = """
         <tr>
@@ -33,11 +35,11 @@ def get(environ):
           <td>{value}</td>
         </tr>
     """
+    rows = ''
 
     for key in sorted(query_dict):
-        table += row.format(param=key, value=query_dict[key])
-
-    return table + '</table></body>'
+        rows += row.format(param=key, value=query_dict[key])
+    return table.format(rows=rows)
 
 def post(environ):
     output = """
@@ -83,32 +85,29 @@ def post(environ):
     )
     name = 'Не указано'
     sex = 'Не указан'
-    education = 'Низшее'
+    education = {
+        'low':'Низшее',
+        'middle':'Среднее',
+        'high':'Высшее'
+    }
     spam = 'Нет'
     comment = post['comment'].value
 
     if post['name'].value:
-        name = post['name'].value
-    
+        name = post['name'].value    
     if 'sex' in post:
         if post['sex'].value == 'female':
             sex = 'Женский'
         else:
-            sex = 'Мужской'
-    
-    if post['education'].value == 'middle':
-        education = 'Среднее'
-    elif post['education'].value == 'high':
-        education = 'Высшее'
-    
+            sex = 'Мужской'    
     if 'spam' in post:
         spam = 'Да'
 
     return output.format(
-        name=name, 
-        sex=sex, 
-        education=education, 
-        comment=comment, 
+        name=name,
+        sex=sex,
+        education=education[post['education'].value],
+        comment=comment,
         spam=spam
     )
 
@@ -117,12 +116,10 @@ def application(environ, start_response):
 
     if environ['REQUEST_METHOD'] == 'POST':
         start_response('200 OK', [('Content-Type','text/html')])
-        return [post(environ).encode('utf-8')]
-    
-    if path == '/hello/':    
+        return [post(environ).encode('utf-8')]    
+    if path == '/hello/':
         start_response('200 OK', [('Content-Type','text/html')])
         return ['Hello Web!'.encode('utf-8')]
-
     if path == '/hello/cookie/':
         page_visits = cookie_counter(environ)
 
@@ -132,14 +129,11 @@ def application(environ, start_response):
             ]
         )
         return ['page_visits = {}'.format(page_visits).encode('utf-8')]
-    
     if path == '/hello/get/':
         start_response('200 OK', [('Content-Type','text/html')])
-        return [get(environ).encode('utf-8')]
-    
+        return [get(environ).encode('utf-8')]    
     if path == '/hello/post/':
         start_response('200 OK', [('Content-Type','text/html')])
         return [(open('form.html', 'rb')).read()]
-    
     start_response('404 Not Found', [('Content-Type','text/html')])
     return ['404 Not Found'.encode('utf-8')]
