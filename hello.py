19d5ef8efb0ce6fa917191991890be78e4bb0fe9
creepysynth <creepysynth@gmail.com>
2017-02-13 16:58:01 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,14 +6,16 @@ def cookie_counter(environ):
     counter = 1
 
     if 'HTTP_COOKIE' in environ:
-        counter = environ['HTTP_COOKIE'].split('=')[1]
-        counter = int(counter) + 1
-    return counter
+        counter = int(environ['HTTP_COOKIE'].split('=')[1]) + 1
+    return (
+        ('200 OK', [('Content-Type','text/html'),
+                    ('Set-Cookie', 'page_visits={}'.format(counter))]),
+        ['page_visits = {}'.format(counter).encode('utf-8')]
+    )
 
 def get(environ):
     query = environ['QUERY_STRING'].split('&')
     query_dict = dict(i.split('=') for i in query)
-
     table = """
         <head>
           <title>GET</title>
@@ -39,101 +41,100 @@ def get(environ):
 
     for key in sorted(query_dict):
         rows += row.format(param=key, value=query_dict[key])
-    return table.format(rows=rows)
+    output = table.format(rows=rows)
+    return (('200 OK', [('Content-Type','text/html')]),
+            [output.encode('utf-8')])
+
+def hello(environ):
+    return (('200 OK', [('Content-Type','text/html')]),
+            ['Hello Web!'.encode('utf-8')])
 
 def post(environ):
-    output = """
-        <head>
-          <meta charset="utf-8">
-          <title>POST</title>
-        </head>
-        <body>
-        <table border="2">
-          <tr>
-            <th>Параметр</th>
-            <th>Значение</th>
-          </tr>
-          <tr>
-            <td>Имя</td>
-            <td>{name}</td>
-          </tr>
-          <tr>
-            <td>Пол</td>
-            <td>{sex}</td>
-          </tr>
-          <tr>
-            <td>Образование</td>
-            <td>{education}</td>
-          </tr>
-          <tr>
-            <td>Комментарий</td>
-            <td>{comment}</td>
-          </tr>
-          <tr>
-            <td>Получать спам</td>
-            <td>{spam}</td>
-            </tr>
-        </table>
-        </body>
-    """
-    post_env = environ.copy()
-    post_env['QUERY_STRING'] = ''
-    post = cgi.FieldStorage(
-        fp=environ['wsgi.input'],
-        environ=post_env,
-        keep_blank_values=True
-    )
-    name = 'Не указано'
-    sex = 'Не указан'
-    education = {
-        'low':'Низшее',
-        'middle':'Среднее',
-        'high':'Высшее'
-    }
-    spam = 'Нет'
-    comment = post['comment'].value
+    if environ['REQUEST_METHOD'] == 'POST':
+        table = """
+            <head>
+              <meta charset="utf-8">
+              <title>POST</title>
+            </head>
+            <body>
+            <table border="2">
+              <tr>
+                <th>Параметр</th>
+                <th>Значение</th>
+              </tr>
+              <tr>
+                <td>Имя</td>
+                <td>{name}</td>
+              </tr>
+              <tr>
+                <td>Пол</td>
+                <td>{sex}</td>
+              </tr>
+              <tr>
+                <td>Образование</td>
+                <td>{education}</td>
+              </tr>
+              <tr>
+                <td>Комментарий</td>
+                <td>{comment}</td>
+              </tr>
+              <tr>
+                <td>Получать спам</td>
+                <td>{spam}</td>
+                </tr>
+            </table>
+            </body>
+        """
+        post = cgi.FieldStorage(
+            fp=environ['wsgi.input'],
+            environ=environ,
+            keep_blank_values=True
+        )
+        name = 'Не указано'
+        sex = 'Не указан'
+        education = {
+            'low':'Низшее',
+            'middle':'Среднее',
+            'high':'Высшее'
+        }
+        spam = 'Нет'
+        comment = post['comment'].value
 
-    if post['name'].value:
-        name = post['name'].value    
-    if 'sex' in post:
-        if post['sex'].value == 'female':
-            sex = 'Женский'
-        else:
-            sex = 'Мужской'    
-    if 'spam' in post:
-        spam = 'Да'
+        if post['name'].value:
+            name = post['name'].value    
+        if 'sex' in post:
+            if post['sex'].value == 'female':
+                sex = 'Женский'
+            else:
+                sex = 'Мужской'    
+        if 'spam' in post:
+            spam = 'Да'
 
-    return output.format(
-        name=name,
-        sex=sex,
-        education=education[post['education'].value],
-        comment=comment,
-        spam=spam
-    )
+        output = table.format(
+            name=name,
+            sex=sex,
+            education=education[post['education'].value],
+            comment=comment,
+            spam=spam
+        )
+        return (('200 OK', [('Content-Type','text/html')]),
+                [output.encode('utf-8')])
+    return (('200 OK', [('Content-Type','text/html')]),
+            [(open('form.html', 'rb')).read()])
+
+def not_found(environ):
+    return (('404 Not Found', [('Content-Type','text/html')]),
+            ['404 Not Found'.encode('utf-8')])
 
 def application(environ, start_response):
     path = environ['PATH_INFO']
+    handler = {
+        '/hello/':hello,
+        '/hello/get/':get,
+        '/hello/cookie/':cookie_counter,
+        '/hello/post/':post
+    }
+    response, data = handler.get(path, not_found)(environ)
 
-    if environ['REQUEST_METHOD'] == 'POST':
-        start_response('200 OK', [('Content-Type','text/html')])
-        return [post(environ).encode('utf-8')]    
-    if path == '/hello/':
-        start_response('200 OK', [('Content-Type','text/html')])
-        return ['Hello Web!'.encode('utf-8')]
-    if path == '/hello/cookie/':
-        page_visits = cookie_counter(environ)
-
-        start_response('200 OK', [
-                ('Content-Type','text/html'),
-                ('Set-Cookie', 'page_visits={}'.format(page_visits))
-            ]
-        )
-        return ['page_visits = {}'.format(page_visits).encode('utf-8')]
-    if path == '/hello/get/':
-        start_response('200 OK', [('Content-Type','text/html')])
-        return [get(environ).encode('utf-8')]    
-    if path == '/hello/post/':
-        start_response('200 OK', [('Content-Type','text/html')])
-        return [(open('form.html', 'rb')).read()]
-    start_response('404 Not Found', [('Content-Type','text/html')])
-    return ['404 Not Found'.encode('utf-8')]
+    start_response(*response)
+    return data
